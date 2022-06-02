0e7fc4f02e89383ec8c65a363cd38e1a8c1ce18c
s2heart1@naver.com <s2heart1@naver.com>
2022-06-02 19:00:20 +0900

    loginpage

---
+++ src/authPages/LoginPage/LoginPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AuthBox from '../../shared/components/AuthBox';
+const LoginPage = () => {
+  return;
+  <AuthBox>LoginPage</AuthBox>;
+};
+
+export default LoginPage;
